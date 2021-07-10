24ff12c9d60dc0dadc47d4d6a727cb934d5b00d1
Hanz <zwillhill@hotmail.com>
2021-07-10 19:56:44 +0800

    Update testgit.sh

--- k3/testgit.sh
+++ k3/testgit.sh
@@ -33,18 +33,20 @@ svn co https://github.com/zwillhill/luci-theme-darkmatter/trunk/luci/themes/luci
 # Add luci-app-bypass
 git clone https://github.com/garypang13/openwrt-bypass.git package/garypang13/openwrt-bypass
 
-# Add bypass depends from xiaorouji
-#svn co https://github.com/xiaorouji/openwrt-passwall/trunk/chinadns-ng package/xiaorouji/chinadns-ng
-#svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-go package/xiaorouji/trojan-go
-#svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-plus package/xiaorouji/trojan-plus
-# Add bypass depends from fw876
-#svn co https://github.com/fw876/helloworld/trunk/naiveproxy package/fw876/naiveproxy
-#svn co https://github.com/fw876/helloworld/trunk/shadowsocksr-libev package/fw876/shadowsocksr-libev
-#svn co https://github.com/fw876/helloworld/trunk/tcping package/fw876/tcping
-#svn co https://github.com/fw876/helloworld/trunk/v2ray-core package/fw876/v2ray-core
-#svn co https://github.com/fw876/helloworld/trunk/v2ray-plugin package/fw876/v2ray-plugin
-#svn co https://github.com/fw876/helloworld/trunk/xray-core package/fw876/xray-core
-#svn co https://github.com/fw876/helloworld/trunk/xray-plugin package/fw876/xray-plugin
+# Add bypass depends
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/chinadns-ng package/xiaorouji/chinadns-ng
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-go package/xiaorouji/trojan-go
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-plus package/xiaorouji/trojan-plus
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/simple-obfs
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk/microsocks
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2
+svn co https://github.com/fw876/helloworld/trunk/naiveproxy package/fw876/naiveproxy
+svn co https://github.com/fw876/helloworld/trunk/shadowsocksr-libev package/fw876/shadowsocksr-libev
+svn co https://github.com/fw876/helloworld/trunk/tcping package/fw876/tcping
+svn co https://github.com/fw876/helloworld/trunk/v2ray-core package/fw876/v2ray-core
+svn co https://github.com/fw876/helloworld/trunk/v2ray-plugin package/fw876/v2ray-plugin
+svn co https://github.com/fw876/helloworld/trunk/xray-core package/fw876/xray-core
+svn co https://github.com/fw876/helloworld/trunk/xray-plugin package/fw876/xray-plugin
 
 # Add luci-apps
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/rufengsuixing/luci-app-adguardhome
