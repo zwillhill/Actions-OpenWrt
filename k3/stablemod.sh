8f462fce959afefdeb9f54c1047fc6beef1d2c6d
Hanz <zwillhill@hotmail.com>
2021-07-21 21:19:01 +0800

    Update stablemod.sh

--- k3/stablemod.sh
+++ k3/stablemod.sh
@@ -20,15 +20,3 @@ sed -i 's|^TARGET_|#TARGET_|g; s|#TARGET_DEVICES += phicomm_k3|TARGET_DEVICES +=
 # Display temperature on index (not work on 19.07)
 #sed -i "/<tr><td width=\"33%\"><%:CPU usage (%)%>/a \ \t\t<tr><td width=\"33%\"><%:CPU Temperature %>（℃）</td><td><%=luci.sys.exec(\"sed 's/../&./g' /sys/class/thermal/thermal_zone0/temp|cut -c1-5\")%>℃</td></tr>"  feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 #============================================================
-#修改校时服务器
-sed -i 's/0.openwrt.pool.ntp.org/ntp.aliyun.com/' package/base-files/files/bin/config_generate
-sed -i 's/1.openwrt.pool.ntp.org/time1.cloud.tencent.com/' package/base-files/files/bin/config_generate
-sed -i 's/2.openwrt.pool.ntp.org/time.ustc.edu.cn/' package/base-files/files/bin/config_generate
-sed -i 's/3.openwrt.pool.ntp.org/cn.pool.ntp.org/' package/base-files/files/bin/config_generate
-#修改banner
-sed -i '1,5d' package/base-files/files/etc/banner
-sed -i '1i\  _______                     ________        __'  package/base-files/files/etc/banner
-sed -i '2i\ |   |   |.---.-.-----.-----.|  |  |  |.----.|  |_ '  package/base-files/files/etc/banner
-sed -i '3i\ |       ||  _  |     |-- __||  |  |  ||   _||   _|'  package/base-files/files/etc/banner
-sed -i '4i\ |___|___||___._|__|__|_____||________||__|  |____|'  package/base-files/files/etc/banner
-#============================================================
