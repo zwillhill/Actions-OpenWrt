7d8019f71e91bccee7f72c1b47e5971261240ae3
Hanz <zwillhill@hotmail.com>
2021-07-22 11:41:06 +0800

    Update stablemod.sh

--- k3/stablemod.sh
+++ k3/stablemod.sh
@@ -14,9 +14,6 @@
 # Modify files permission
 #chmod -R 755 files
 #============================================================
-# Only compile K3
-sed -i 's|^TARGET_|#TARGET_|g; s|#TARGET_DEVICES += phicomm_k3|TARGET_DEVICES += phicomm_k3|' target/linux/bcm53xx/image/Makefile
-#============================================================
 # Display temperature on index (not work on 19.07)
 #sed -i "/<tr><td width=\"33%\"><%:CPU usage (%)%>/a \ \t\t<tr><td width=\"33%\"><%:CPU Temperature %>（℃）</td><td><%=luci.sys.exec(\"sed 's/../&./g' /sys/class/thermal/thermal_zone0/temp|cut -c1-5\")%>℃</td></tr>"  feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 #============================================================
