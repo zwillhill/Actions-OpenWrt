1e6ffbf610c22eb8ed0564eb62ae7696fd3a7d53
Hanz <zwillhill@hotmail.com>
2021-07-22 11:49:16 +0800

    Update stablegit.sh

--- k3/stablegit.sh
+++ k3/stablegit.sh
@@ -16,8 +16,7 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #============================================================
 # Only compile K3
-sed -i 's|^TARGET_|#TARGET_|g; s|#TARGET_DEVICES += phicomm-k3|TARGET_DEVICES += phicomm-k3|' target/linux/bcm53xx/image/Makefile
-sed -i 's|^TARGET_|#TARGET_|g; s|#TARGET_DEVICES += phicomm_k3|TARGET_DEVICES += phicomm_k3|' target/linux/bcm53xx/image/Makefile
+sed -i 's|^TARGET_|#TARGET_|g; s|#TARGET_DEVICES += phicomm|TARGET_DEVICES += phicomm|' target/linux/bcm53xx/image/Makefile
 #============================================================
 # use luci 19.07
 #sed -i 's/luci.git/luci.git;openwrt-19.07/' feeds.conf.default
